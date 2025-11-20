20dc73fa67d3fd354929a5108a56fab8474e62c8
Aravind B <bettercallaravindan@gmail.com>
2025-11-20 08:23:51 +0530

    Initial commit

---
+++ backend/src/main/java/com/aravind/booster/Application.java
@@ -0,0 +1,13 @@
+package com.aravind.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("🚀 The Daily GK Express Backend is Running...");
+    }
+}
