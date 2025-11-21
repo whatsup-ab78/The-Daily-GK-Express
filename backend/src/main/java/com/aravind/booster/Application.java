9472e3a03a388e886fdb60361a7997b4f2b39997
Aravind B <bettercallaravindan@gmail.com>
2025-11-21 21:06:55 +0530

    Docker and PostgreSQL implemented successfully

--- backend/src/main/java/com/aravind/booster/Application.java
+++ backend/src/main/java/com/aravind/booster/Application.java
@@ -3,6 +3,8 @@ package com.aravind.booster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableScheduling   
@@ -11,5 +13,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("🚀 The Daily GK Express Backend is Running...");
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
